73dd1baaf9dc475511b9f87093bf7aca8eb2a359
amehta85 <aanal.mehta@outlook.com>
2024-10-28 15:17:32 -0700

    Added code for balanced binary tree

---
+++ src/BalancedBinaryTree.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        
+        def check_height(node):
+            if not node:
+                return 0 
+            
+            left_height = check_height(node.left)
+            if left_height == -1:  
+                return -1
+            
+            right_height = check_height(node.right)
+            if right_height == -1:  
+                return -1
+            
+            if abs(left_height - right_height) > 1:
+                return -1  
+            
+            return max(left_height, right_height) + 1  
+
+        return check_height(root) != -1  
